a7fbdd071c0b420b05618034588417682102d5a9
ngocdv <ngocdv@ghn.vn>
2022-12-23 14:55:25 +0700

    edit

--- extra/rediscmd/safe.go
+++ extra/rediscmd/safe.go
@@ -1,6 +1,3 @@
-//go:build appengine
-// +build appengine
-
 package rediscmd
 
 func String(b []byte) string {
--- internal/util/safe.go
+++ internal/util/safe.go
@@ -1,6 +1,3 @@
-//go:build !appengine
-// +build !appengine
-
 package util
 
 func BytesToString(b []byte) string {
